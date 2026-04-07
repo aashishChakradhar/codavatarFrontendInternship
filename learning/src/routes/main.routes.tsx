98fcff56f9883da3997c842720d58366b7bd650d
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-04-07 11:18:19 +0545

    add lazy, suspense

--- learning/src/routes/main.routes.tsx
+++ learning/src/routes/main.routes.tsx
@@ -1,21 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../layout/layout";
+import { lazy, Suspense } from "react";
 
-import Form from "../pages/form/form.pages";
-import Submit from "../pages/submit.page";
-import TodoList from "../pages/todo/todo.page";
-import JunkApp from "../pages/junk/junk.page";
+const Form = lazy(() => import("../pages/form/form.pages"));
+const Submit = lazy(() => import("../pages/submit.page"));
+const TodoList = lazy(() => import("../pages/todo/todo.page"));
+const JunkApp = lazy(() => import("../pages/junk/junk.page"));
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route key="home" path="/" element={<Form />} />
-        <Route key="home" path="form" element={<Form />} />
-        <Route key="submit" path="/submit" element={<Submit />} />
-        <Route key="to" path="/todo" element={<TodoList />} />
-        <Route key="junk" path="/junk" element={<JunkApp />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<h2>Loading Page...</h2>}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route key="home" path="/" element={<Form />} />
+          <Route key="home" path="form" element={<Form />} />
+          <Route key="submit" path="/submit" element={<Submit />} />
+          <Route key="to" path="/todo" element={<TodoList />} />
+          <Route key="junk" path="/junk" element={<JunkApp />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
