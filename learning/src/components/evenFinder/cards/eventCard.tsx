8fee36dff4071a5d9c4d1a905877b48626a38cae
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-04-22 14:35:27 +0545

    created events card

---
+++ learning/src/components/evenFinder/cards/eventCard.tsx
@@ -0,0 +1,73 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardActions from "@mui/material/CardActions";
+import image from "../../../assets/react.svg";
+// const image = "../..";
+
+export interface CardProp {
+  alt: string;
+  title: string;
+  description: string;
+  date: string;
+  place: string;
+}
+
+export default function EventCard({
+  alt,
+  title,
+  description,
+  date,
+  place,
+}: CardProp) {
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        backgroundColor: "transparent",
+        border: "2px solid rgba(0,0,0,0.2)",
+        borderRadius: "8px",
+        boxShadow: " 0px 10px 10px 5px rgba(0, 0, 0, 0.15)",
+      }}
+    >
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image={image}
+          alt={alt}
+          sx={{ height: "150px", objectFit: "contain", padding: "15px" }}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="subtitle1"
+            component="div"
+            sx={{ fontWeight: "bold" }}
+          >
+            {title}
+            <div className="text-sm font-normal italic">
+              {date}: {place}
+            </div>
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="subtitle2"
+            component="div"
+            sx={{ fontStyle: "italic" }}
+          ></Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" variant="outlined">
+          Learn More {">>"}
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
