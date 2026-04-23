8be9b0208b21507447e3a1e2a8c7310b9a07bd0b
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-04-23 14:55:13 +0545

    add: helper functions

--- learning/src/utils/eventFinder/eventFinder.ts
+++ learning/src/utils/eventFinder/eventFinder.ts
@@ -1,19 +1,20 @@
 import type { Dayjs } from "dayjs";
 import data from "../../data/eventFinder/events.json";
+import dayjs from "dayjs";
 
-type EventItem = {
+export interface EventItemProp {
   id: number;
   name: string;
   date: string;
   time: string;
   description: string;
   place: string;
-};
+}
 
-function loadData(): EventItem[] {
+function loadData(): EventItemProp[] {
   //checks if data is array
-  //keep it in EventItem structure
-  const eventList = Array.isArray(data) ? (data as EventItem[]) : [];
+  //keep it in EventItemProp structure
+  const eventList = Array.isArray(data) ? (data as EventItemProp[]) : [];
   return eventList.toSorted((first, second) =>
     first.date.localeCompare(second.date),
   );
@@ -29,10 +30,24 @@ export function loadEventDates(date: Dayjs): string[] {
   return dates;
 }
 
-export function loadEventData(date: string): EventItem[] | undefined {
+export function findEvent(id: number): EventItemProp | undefined {
+  const eventList = loadData();
+  const event = eventList.find((evnt) => evnt.id === id);
+  console.log(event);
+  return event;
+}
+
+export function loadEventData(date: string): EventItemProp[] | undefined {
   const eventList = loadData();
   //   search data based on date
   //returns object
   const available = eventList.filter((event) => event.date === date);
   return available;
+}
+
+export function findUpcomingEvents(): EventItemProp[] {
+  const loadedData = loadData();
+  const date = dayjs().format("YYYY-MM-DD");
+  const upcoming = loadedData.filter((load) => load.date >= date);
+  return upcoming.slice(0, 5);
 }
