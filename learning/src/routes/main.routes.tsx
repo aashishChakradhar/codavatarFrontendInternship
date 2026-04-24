57afe4f61005e4731647a6d47fa8d85048e826df
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-04-24 14:23:35 +0545

    routes and setup

--- learning/src/routes/main.routes.tsx
+++ learning/src/routes/main.routes.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { allRoutes } from "./allroute";
 import { eventFinderRoutes } from "./evenFinder/eFinderRoutes";
+import { pokemonRoutes } from "./pokemon/pokemonRoutes";
 
-const mainRouter = createBrowserRouter([...allRoutes, ...eventFinderRoutes]);
+const mainRouter = createBrowserRouter([
+  ...allRoutes,
+  ...eventFinderRoutes,
+  ...pokemonRoutes,
+]);
 
 export default mainRouter;
