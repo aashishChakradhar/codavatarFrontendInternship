4d0820b677812ad96763755762dc09a6c31f434c
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-04-24 14:21:05 +0545

    setup: apollo client

--- learning/src/main.tsx
+++ learning/src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://graphql-pokeapi.vercel.app/api/graphql" }),
+  cache: new InMemoryCache(),
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 );
