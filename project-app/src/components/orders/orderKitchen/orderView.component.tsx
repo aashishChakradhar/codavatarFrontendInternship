0efa58f28c26c749ae39179fad0e4a7e63391ae5
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-05-11 13:09:04 +0545

    create content for kitchen order

--- project-app/src/components/orders/orderKitchen/orderView.component.tsx
+++ project-app/src/components/orders/orderKitchen/orderView.component.tsx
@@ -1,1 +1,162 @@
-export function OrderViewComponent() {}
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { orderStateColor, type OrderStateType } from "@/constants/constants"
+import type { OrderDataInterface } from "@/data/orderData"
+import { changeOrderState } from "@/redux/order/orderSlice"
+import type { AppDispatch } from "@/redux/store"
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { ConfirmChange } from "../orderRestro/changeOrder"
+
+export function OrderKitchenComponent({
+  orderId,
+  items,
+  states,
+  group,
+}: {
+  orderId: number
+  items: OrderDataInterface[]
+  states: OrderStateType[]
+  group: "order" | "items"
+}) {
+  const [open, setOpen] = useState<boolean>(false)
+  const [selectedOrder, setSelectedOrder] = useState<OrderDataInterface | null>(
+    null
+  )
+  const [selectedState, setSelectedState] = useState<OrderStateType | null>(
+    null
+  )
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  const handleClick = (order: OrderDataInterface, state: OrderStateType) => {
+    setSelectedOrder(order)
+    setSelectedState(state)
+    setOpen(true)
+  }
+
+  const handleConfirm = () => {
+    if (selectedOrder && selectedState) {
+      dispatch(
+        changeOrderState({ order: selectedOrder, orderState: selectedState })
+      )
+    }
+    setOpen(false)
+    setSelectedOrder(null)
+    setSelectedState(null)
+  }
+
+  return (
+    <Card className="relative mx-auto w-full max-w-xs">
+      <CardHeader>
+        <div className="flex justify-between">
+          <CardTitle>
+            {group === "order" ? <> Order: {orderId}</> : <>Item: {orderId}</>}
+          </CardTitle>
+          <Badge variant="secondary">Items: x{items.length}</Badge>
+        </div>
+
+        <CardDescription>
+          {items.map((item) => (
+            <div className="flex items-center justify-between">
+              <div>
+                {item.name} x{item.quantity}
+              </div>
+              <DropdownMenu key={item.itemId}>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    className={`${orderStateColor(item.state)} rounded-sm`}
+                  >
+                    {item.state}
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  {states.map((s) => (
+                    <DropdownMenuItem
+                      key={s}
+                      onClick={() => handleClick(item, s)}
+                      hidden={s === item.state}
+                    >
+                      {s}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          ))}
+        </CardDescription>
+      </CardHeader>
+      {/* <CardFooter className="flex flex-col gap-2">
+        {items.map((order) => (
+          <DropdownMenu key={order.itemId}>
+            <DropdownMenuTrigger asChild>
+              <Button className={`${orderStateColor(order.state)} w-full`}>
+                {order.name} - {order.state}
+                <span className="sr-only">{order.state}</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {states.map((s) => (
+                <DropdownMenuItem
+                  key={s}
+                  onClick={() => handleClick(order, s)}
+                  hidden={s === order.state}
+                >
+                  {s}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ))}
+      </CardFooter> */}
+      <ConfirmChange
+        open={open}
+        onOpenChange={setOpen}
+        state={selectedState ?? "pending"}
+        onConfirm={handleConfirm}
+      />
+    </Card>
+  )
+}
+
+export function CardImage() {
+  return (
+    <Card className="relative mx-auto w-full max-w-sm pt-0">
+      <div className="absolute inset-0 z-30 aspect-video bg-black/35" />
+      <img
+        src="https://avatar.vercel.sh/shadcn1"
+        alt="Event cover"
+        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+      />
+      <CardHeader>
+        <CardAction>
+          <Badge variant="secondary">Featured</Badge>
+        </CardAction>
+        <CardTitle>Design systems meetup</CardTitle>
+        <CardDescription>
+          A practical talk on component APIs, accessibility, and shipping
+          faster.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Button className="w-full">View Event</Button>
+      </CardFooter>
+    </Card>
+  )
+}
--- project-app/src/components/orders/orderKitchen/orderView.container.tsx
+++ project-app/src/components/orders/orderKitchen/orderView.container.tsx
@@ -1,4 +1,155 @@
+import { useDispatch, useSelector } from "react-redux"
+import { OrderKitchenComponent } from "./orderView.component"
+import type { AppDispatch, RootState } from "@/redux/store"
+import { fetchOrder } from "@/redux/order/orderSlice"
+import { useEffect, useState } from "react"
+import type { OrderDataInterface } from "@/data/orderData"
+import { orderState, type OrderStateType } from "@/constants/constants"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+function orderArrange(orderList: OrderDataInterface[]) {
+  const grouped: Record<number, OrderDataInterface[]> = {}
+
+  orderList.forEach((order) => {
+    if (!grouped[order.orderId]) {
+      grouped[order.orderId] = []
+    }
+    grouped[order.orderId].push(order)
+  })
+
+  return Object.entries(grouped).map(([orderId, items]) => ({
+    [Number(orderId)]: items,
+  }))
+}
+
+function itemArrange(orderList: OrderDataInterface[]) {
+  const grouped: Record<number, OrderDataInterface[]> = {}
+
+  orderList.forEach((order) => {
+    if (!grouped[order.itemId]) {
+      grouped[order.itemId] = []
+    }
+    grouped[order.itemId].push(order)
+  })
+
+  return Object.entries(grouped).map(([itemId, items]) => ({
+    [Number(itemId)]: items,
+  }))
+}
+
 function OrderKitchen() {
-  return <div>Kitchen Container</div>
+  const dispatch = useDispatch<AppDispatch>()
+  const { orders, status, error } = useSelector(
+    (state: RootState) => state.order
+  )
+  const user = useSelector((state: RootState) => state.user)
+  const [availableOrders, setAvailableOrders] = useState<OrderDataInterface[]>(
+    []
+  )
+  const [availableOrdersState, setAvailableState] = useState<OrderStateType[]>([
+    ...orderState,
+  ])
+  const [groupBy, setGroupBy] = useState<"order" | "items">("order")
+  const [arrangedOrder, setArrangedOrder] = useState<
+    { [key: number]: OrderDataInterface[] }[]
+  >([])
+
+  //fetch order data
+  useEffect(() => {
+    if (status === "idle") dispatch(fetchOrder())
+  }, [dispatch, status])
+
+  //assign order state
+  useEffect(() => {
+    if (user.isAdmin) {
+      setAvailableState([...orderState])
+    }
+    if (user.role === "kitchen") {
+      setAvailableState(["preparing", "completed"])
+    }
+  }, [user.isAdmin, user.role])
+
+  // filter perticular available orders
+  useEffect(() => {
+    if (user.isAdmin) {
+      setAvailableOrders([...orders])
+      setArrangedOrder(orderArrange(orders))
+    }
+    if (user.role === "kitchen") {
+      const filteredOrders = orders.filter(
+        (order) => order.state === "preparing" || order.state === "pending"
+      )
+      setAvailableOrders(filteredOrders)
+      setArrangedOrder(orderArrange(filteredOrders))
+    }
+  }, [user.isAdmin, user.role, orders])
+
+  if (status === "pending") {
+    return <div className="text-sm">Loading Menu...</div>
+  }
+
+  if (status === "failed") {
+    return (
+      <div className="text-sm text-red-500">
+        Error loading Menu
+        <hr />
+        {error}
+      </div>
+    )
+  }
+
+  const onArrangeOrder = () => {
+    setGroupBy("order")
+    setArrangedOrder(orderArrange(availableOrders))
+  }
+
+  const onArrangeItems = () => {
+    setGroupBy("items")
+    setArrangedOrder(itemArrange(availableOrders))
+  }
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Sort</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-40" align="start">
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={onArrangeOrder}>
+              By Order
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={onArrangeItems}>
+              By Items
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="flex flex-row flex-wrap items-start justify-around gap-5">
+        {arrangedOrder.map((group) => {
+          const [groupId, items] = Object.entries(group)[0]
+          return (
+            <OrderKitchenComponent
+              key={groupId}
+              orderId={Number(groupId)}
+              items={items}
+              states={availableOrdersState}
+              group={groupBy}
+            />
+          )
+        })}
+      </div>
+    </>
+  )
 }
 export default OrderKitchen
