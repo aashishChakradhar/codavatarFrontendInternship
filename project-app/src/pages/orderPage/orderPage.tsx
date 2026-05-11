93ac0833c0b5019331307375914213961148b80f
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-05-11 09:05:05 +0545

    rename page

--- project-app/src/pages/orderPage/orderPage.tsx
+++ project-app/src/pages/orderPage/orderPage.tsx
@@ -1,5 +1,14 @@
-import OrderView from "@/components/orderView"
+import OrderRestro from "@/components/orders/orderRestro"
+import OrderKitchen from "@/components/orders/orderKitchen"
+import type { RootState } from "@/redux/store"
+import { useSelector } from "react-redux"
 
-export default function orderPage() {
-  return <OrderView />
+export default function OrderPage() {
+  const user = useSelector((state: RootState) => state.user)
+  return (
+    <>
+      {user.role === "restro" && <OrderRestro />}
+      {user.role === "kitchen" && <OrderKitchen />}
+    </>
+  )
 }
---
+++ project-app/src/pages/orderPage/orderRestroPage.tsx
@@ -0,0 +1,5 @@
+import OrderRestro from "@/components/orders/orderRestro"
+
+export default function OrderPage() {
+  return <OrderRestro />
+}
