9ff84d84afa1f5ec6498841ab4cea9c0c1f0bafb
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-05-19 13:38:47 +0545

    add confirmation alert

--- project-app/src/components/orders/orderCart/orderSummary.tsx
+++ project-app/src/components/orders/orderCart/orderSummary.tsx
@@ -19,18 +19,32 @@ import { useState } from "react"
 
 import ChairIcon from "@mui/icons-material/Chair"
 import { SelectOrderTable } from "./orderTableSelect"
-import { ConfirmOrder } from "./confirmOrder"
-import useLoadTable from "@/hooks/use-loadTable"
+import { ConfirmAlert } from "@/components/alert/confirmAlert"
+import { useCreateOrder } from "@/hooks/useCreateOrder"
 
-// export function DrawerWithSides() {
 function PlaceOrder() {
   const { tables } = useSelector((state: RootState) => state.table)
-  const { orderTemp } = useSelector((state: RootState) => state.order)
+  const selectedTable = useSelector(
+    (state: RootState) => state.table.selectedTable
+  )
+  const createItem = useSelector(
+    (state: RootState) => state.item.createItemData
+  )
   const [open, setOpen] = useState<boolean>(false)
   const [orderOpen, setOrderOpen] = useState<boolean>(false)
-  const hasOrder: boolean = orderTemp.length !== 0
+  const hasNoOrder: boolean = createItem.length === 0
+  const hasNoTable: boolean = selectedTable.number === 0
 
-  useLoadTable()
+  const selectOrder = useCreateOrder()
+
+  const confirmOrder = async () => {
+    try {
+      await selectOrder()
+      setOpen(false)
+    } catch (err) {
+      console.error("ConfirmOrder.selectOrder error:", err)
+    }
+  }
 
   return (
     <div className="flex flex-wrap gap-2">
@@ -63,17 +77,28 @@ function PlaceOrder() {
           </div>
 
           <DrawerFooter>
-            <Button onClick={() => setOpen(true)} disabled={!hasOrder}>
+            <Button
+              onClick={() => setOpen(true)}
+              disabled={hasNoOrder || hasNoTable}
+            >
               Place Order
             </Button>
-            <ConfirmOrder open={open} onOpenChange={setOpen} />
-
             <DrawerClose asChild>
               <Button variant="outline">Cancel</Button>
             </DrawerClose>
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
+
+      <ConfirmAlert
+        onConfirm={confirmOrder}
+        open={open}
+        onOpenChange={setOpen}
+        alertTitle="Confirm Order?"
+        alertMessage={`Place order for given details:${" "}
+                Section: ${selectedTable.section.name.toUpperCase()}
+                Table: ${selectedTable.number}`}
+      />
     </div>
   )
 }
