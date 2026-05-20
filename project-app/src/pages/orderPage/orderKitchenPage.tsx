148f82cdcbef0f232a2e16994b175d1d699763f4
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-05-20 13:49:55 +0545

    hook to load data

--- project-app/src/pages/orderPage/orderKitchenPage.tsx
+++ project-app/src/pages/orderPage/orderKitchenPage.tsx
@@ -1,5 +1,7 @@
 import OrderKitchen from "@/components/orders/orderKitchen"
+import useLoadOrder from "@/hooks/use-loadOrder"
 
 export default function OrderPage() {
+  useLoadOrder()
   return <OrderKitchen />
 }
