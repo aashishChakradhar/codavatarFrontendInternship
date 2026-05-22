9f3752c8ecc06738220c776a8e29b677accec20c
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-05-22 14:37:52 +0545

    create: dashboard page

---
+++ project-app/src/pages/dashboardPage/dashboardPage.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "@/components/dashboard"
+
+export default function DashboardPage() {
+  console.log("here Page")
+  return (
+    <>
+      Dashboard Page
+      <Dashboard />
+    </>
+  )
+}
