32a3e6c465993b0bcbd93b5d574743e03e0c8040
Aashish Chakradhar <ashishchakradhar00@gmail.com>
2026-05-22 14:38:35 +0545

    create: billing/order checkout page

--- project-app/src/pages/billingPage/billingPage.tsx
+++ project-app/src/pages/billingPage/billingPage.tsx
@@ -1,6 +1,14 @@
 import Billing from "@/components/billingPage"
+import useBill from "@/hooks/useBill"
+import { useEffect } from "react"
 
 export default function BillingPage() {
+  const { readBill } = useBill()
+
+  useEffect(() => {
+    void readBill()
+  }, [readBill])
+
   return (
     <>
       <Billing />
